76e6a66daa6a5bc56c8d4d67bd4246e1434c5838
Bryan <bryanlu.my@gmail.com>
2023-05-18 17:38:23 +0800

    initial commit migrated from main page

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,23 +1,111 @@
-import { Component } from "react";
-import { ContentDisplay } from "../components/others/";
-import { Error404Page } from "../components/pages";
+import { Component, createRef } from "react";
+import { ContentDisplay } from "../components/others";
 
-export default class Main extends Component {
+export default class FlipACoin extends Component {
+	constructor(props) {
+		super(props);
+		this.coinRef = createRef();
+		this.state = {
+			isFlippingButtonClickable: true
+		};
+	}
+
+	handleCoinFlipButton = () => {
+		if (!this.state.isFlippingButtonClickable)
+			return;
+		this.setState({ isFlippingButtonClickable: false });
+		this.coinRef.current.flipCoin();
+	};
+
+	setIsFlippingButtonClickable = () => {
+		this.setState({ isFlippingButtonClickable: true });
+	};
+
 	render() {
 		return (
 			<ContentDisplay
-				backButtonRoute={"https://bryanluwz.github.io/#/"}
-				displayName={Main.displayName}
+				backButtonRoute={"https://bryanluwz.github.io/#/fun-stuff"}
+				displayName={FlipACoin.displayName}
 				displayClearHistory={false}
-				faIcon={"fa-trash"}
-				contentBodyAdditionalClasses={["cat-gpt-content-wrapper"]}
+				faIcon="fa-plane"
+				contentBodyAdditionalClasses={[]}
 				router={this.props.router}
-				handleHeaderTitleClick={() => { console.log("please do not the cat"); }}
+				handleHeaderTitleClick={() => { ; }}
+				handleDeleteHistoryButton={() => { ; }}
 			>
-				<Error404Page customWarning={"well... this is awkward..."} />
+				<div className="fac-wrapper">
+					<div className="fac-container">
+						<Coin ref={this.coinRef} setIsFlippingButtonClickable={this.setIsFlippingButtonClickable} handleCoinFlip={this.handleCoinFlipButton} />
+						<div className="fac-buttons">
+							<button className={`fac-button ${!this.state.isFlippingButtonClickable ? "fac-button-flipping" : ""}`} onClick={this.handleCoinFlipButton}>
+								Flip
+							</button>
+						</div>
+					</div>
+				</div>
 			</ContentDisplay>
 		);
 	}
 }
 
-Main.displayName = "Display Name";
+FlipACoin.displayName = "Flip A Coin";
+
+class Coin extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isFlipping: false,
+			coinSide: Math.random() > 0.5 ? "heads" : "tails",
+			isIdle: true,
+			consecSameSide: 0
+		};
+	}
+
+	flipCoin = () => {
+		if (this.state.isFlipping) return;
+
+		this.setState({ isFlipping: true, isIdle: false });
+
+		setTimeout(() => {
+			const prevCoinSide = this.state.coinSide;
+			const coinSide = Math.random() > 0.5 ? "heads" : "tails";
+
+			if (coinSide === prevCoinSide) {
+				this.setState((prevState) => ({ consecSameSide: prevState.consecSameSide + 1 }));
+			}
+			else {
+				this.setState({ consecSameSide: 1 });
+			}
+
+			this.setState({ coinSide: coinSide, isFlipping: false });
+			setTimeout(() => {
+				this.setState({ isIdle: true });
+				setTimeout(() => {
+					this.props.setIsFlippingButtonClickable();
+				}, 200);
+			}, 500);
+		}, 1000);
+	};
+
+	getCoinSide() {
+		return this.state.coinSide;
+	}
+
+	render() {
+		return (
+			<div className="fac-coin-container">
+				<div className={`fac-coin ${this.state.isIdle ? "facCoinHover" : this.state.isFlipping ? "facCoinOut" : "facCoinIn"}`}
+					onClick={this.props.handleCoinFlip}>
+					<img style={{ display: `${this.state.coinSide === 'heads' ? '' : 'none'}` }} className={`${this.state.isFlipping ? "facCoinOut" : "facCoinIn"}`} src={process.env.PUBLIC_URL + "/images/Flip-A-Coin-assets/FAC-Heads.png"} alt="heads" />
+					<img style={{ display: `${this.state.coinSide === 'tails' ? '' : 'none'}` }} className={`${this.state.isFlipping ? "facCoinOut" : "facCoinIn"}`} src={process.env.PUBLIC_URL + "/images/Flip-A-Coin-assets/FAC-Tails.png"} alt="tails" />
+				</div>
+
+				<div className={`fac-coin-side-indicator ${this.state.isFlipping ? "fadeOut" : "fadeIn"}`}>
+					{this.state.consecSameSide > 2 ? "🔥".repeat(Math.min(2, this.state.consecSameSide)) + " " : ""}
+					{this.state.coinSide + (this.state.consecSameSide > 1 ? ` x${this.state.consecSameSide}` : "")}
+					{this.state.consecSameSide > 2 ? " " + "🔥".repeat(Math.min(2, this.state.consecSameSide)) : ""}
+				</div>
+			</div>
+		);
+	}
+}
